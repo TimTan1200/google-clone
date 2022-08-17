3a9511d9e540a82d915271c1671c88841233089d
TimTan <beasttran018@gmail.com>
2022-08-17 23:01:04 +0800

    Made it look much better

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,12 @@ export default function Home() {
   const [link, setLink] = useState('');
   const [count, setCount] = useState(typeof window !== 'undefined' ? localStorage.getItem('count') : 0);
 
+  // Console Log
+
+  useEffect(() => {
+    console.log("Engine changed to " + engine);
+  }, [engine]);
+
   // Functions
 
   function timer () {
@@ -101,7 +107,9 @@ export default function Home() {
 
   // Background Image Handling
 
-  const [url, setURL] = useState(typeof window !== 'undefined' ? (localStorage.getItem('bg') ? localStorage.getItem('bg') : "https://images.unsplash.com/photo-1643228995868-bf698f67d053") : "https://images.unsplash.com/photo-1643228995868-bf698f67d053");
+  var mainBg = "https://images.unsplash.com/photo-1572816703439-d8b34c4dc93f"
+
+  const [url, setURL] = useState(typeof window !== 'undefined' ? (localStorage.getItem('bg') ? localStorage.getItem('bg') : mainBg) : mainBg);
 
   // Quote Handling
 
@@ -136,12 +144,16 @@ export default function Home() {
       null
     } else { 
       if (engine == 'google') {
+        console.log('Engine: Google');
         window.location.href = 'https://www.google.com/search?q=' + prompt;
       } else if (engine == 'duckduckgo') {
+        console.log('Engine: DuckDuckGo');
         window.location.href = 'https://duckduckgo.com/?q=' + prompt;
       } else if (engine == 'bing') {
+        console.log('Engine: Bing');
         window.location.href = 'https://www.bing.com/search?q=' + prompt;
       } else {
+        console.log('Engine: Google');
         window.location.href = 'https://www.google.com/search?q=' + prompt;
       }
     }
@@ -149,7 +161,7 @@ export default function Home() {
 
   function openURL(url) {
     if (url == '' || url == ' ' || url == undefined) {
-      null
+      console.log('URL empty');
     } else {
         if (typeof window !== 'undefined') {
           window.location.href = url;
@@ -173,55 +185,73 @@ export default function Home() {
         localStorage.setItem('engine', engine);
         localStorage.setItem('links', JSON.stringify(links));
         localStorage.setItem('bg', url);
+
+        console.log('Saved state');
       }
   };
 
   return (
     <div className='w-screen h-screen relative overflow-hidden'>
       <div className='w-screen h-screen absolute top-0 left-0 bg-cover z-0' style={{backgroundImage: "url('" + url + "')"}}></div>
+
       <div className='w-screen h-screen absolute top-0 left-0 z-20'>
         <div className='flex h-full'>
-          <div className='m-auto xl:max-w-xl 2xl:max-w-2xl max-w-lg animate__animated animate__fadeIn'>
-            <form className='w-full' onSubmit={(e) => {e.preventDefault(), setSearch()}}>
-              <input onChange={handleChange} value={input} type='text' style={{backgroundColor: searchColorO, color: stextColor}} className='w-[516px] rounded-lg p-2.5' placeholder='Search' />
+          <div className='m-auto xl:max-w-xl 2xl:max-w-2xl max-w-lg px-4 w-full text-center animate__animated animate__fadeIn'>
+            <span className='text-5xl font-bold font-serif text-center' style={{color: bgColor}}>Googl</span>
+            <form className='max-w-lg w-full mx-auto mt-4 relative flex hover:scale-105 transition-all ease-in' onSubmit={(e) => {e.preventDefault(), setSearch()}}>
+              <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" style={{color: stextColor}} fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-search"><circle cx="11" cy="11" r="8"/><line x1="21" y1="21" x2="16.65" y2="16.65"/></svg>
+              </div>
+              <input onChange={handleChange} value={input} type='text' style={{backgroundColor: searchColorO, color: stextColor}} className='w-full rounded-lg p-2.5 pl-10 border-2 border-white focus:border-gray-700 outline-none' />
             </form>
           </div>
         </div>
       </div>
 
       <div className='w-12 h-10 absolute top-6 right-0 z-30 rounded-l-lg' style={{backgroundColor: bgColorO, color: textColor}}>
-        <div className='flex h-full items-center p-2 justify-between'>
+        <div className={` ${menu ? "hidden" : "block flex h-full items-center p-2 justify-between"} `}>
           <svg onClick={() => toggleMenu(n => !n)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-grid hidden"><rect x="3" y="3" width="7" height="7"/><rect x="14" y="3" width="7" height="7"/><rect x="14" y="14" width="7" height="7"/><rect x="3" y="14" width="7" height="7"/></svg>
-          <svg onClick={() => toggleMenu(n => !n)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"/><line x1="3" y1="6" x2="21" y2="6"/><line x1="3" y1="18" x2="21" y2="18"/></svg>
+          <svg onClick={() => toggleMenu(n => !n)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="3" y1="12" x2="21" y2="12"/><line x1="3" y1="6" x2="21" y2="6"/><line x1="3" y1="18" x2="21" y2="18"/></svg>
         </div>
       </div>
 
-      <div className={`max-w-xs h-96 w-full z-50 absolute top-24 right-4 rounded-lg p-4 bg-[#101010] text-[#f4f4f4] ${menu ? "block animate__animated animate__fadeInRight" : "animate__animated animate__fadeOutRight"}`}>
-        <div className='flex justify-between'>
-          <h1 className='text-2xl font-bold'>Quick Links</h1>
-          <svg onClick={() => toggleAdd(n => !n)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-plus-circle"><circle cx="12" cy="12" r="10"/><line x1="12" y1="8" x2="12" y2="16"/><line x1="8" y1="12" x2="16" y2="12"/></svg>
-        </div>
-        <hr className='my-1'></hr> 
-        <div className='space-y-4 overflow-y-scroll h-[300px] pr-2'>
-          <div className={`space-y-2 pr-2 py-1 ${add ? "animate__animated animate__fadeInDown block" : "animate__animated animate__fadeOutUp hidden"}`}>
-            <div className='w-full'>
-              <p className='text-sm'>Name</p>
-              <input onChange={(e) => setName(e.target.value)} value={name} type='text' className='bg-gray-50 text-black w-full rounded-lg text-base p-2 border border-gray-300 transition ease-in focus:outline-none' placeholder='Name' />
-            </div>
-            <div className='w-full'>
-              <p className='text-sm'>Link URL</p>
-              <input onChange={(e) => setLink(e.target.value)} value={link} type='text' className='bg-gray-50 text-black w-full rounded-lg text-base p-2 border border-gray-300 transition ease-in focus:outline-none' placeholder='URL' />
-            </div>
-            <button onClick={() => addLink()} type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Add Links</button>
-            <hr className='my-1'></hr>
+      <div className={`max-w-xs h-2/3 w-full z-50 flex absolute top-6 right-0 ${menu ? "block animate__animated animate__slideInRight" : "animate__animated animate__slideOutRight animate__fastest "}`}>
+        <div className='w-12 h-10 rounded-l-lg bg-white text-black'>
+          <div className='flex h-full items-center p-2 justify-between'>
+            <svg onClick={() => toggleMenu(n => !n)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-plus-circle rotate-45"><circle cx="12" cy="12" r="10"/><line x1="12" y1="8" x2="12" y2="16"/><line x1="8" y1="12" x2="16" y2="12"/></svg>
           </div>
+        </div>
+        <div className='rounded-b-lg rounded-tr-lg p-4 bg-white text-black w-full'>
+          <div className='flex justify-between sticky'>
+            <h1 className='text-2xl font-bold'>Quick Links</h1>
+            <svg onClick={() => toggleAdd(n => !n)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-plus-circle"><circle cx="12" cy="12" r="10"/><line x1="12" y1="8" x2="12" y2="16"/><line x1="8" y1="12" x2="16" y2="12"/></svg>
+          </div>
+          <hr className='my-1'></hr> 
+          <div className='space-y-4 overflow-y-scroll h-full pr-2 relative'>
+            <div className={`space-y-2 w-full pr-2 py-1 bg-white ${add ? "animate__animated animate__fadeInDown" : "animate__animated animate__fadeOutUp"}`}>
+              <div className='w-full'>
+                <p className='text-sm'>Title</p>
+                <input onChange={(e) => setName(e.target.value)} value={name} type='text' className='bg-gray-50 text-black w-full rounded-lg text-base p-2 border border-gray-300 transition ease-in focus:outline-none' placeholder='Name' />
+              </div>
+              <div className='w-full'>
+                <p className='text-sm'>Link URL</p>
+                <input onChange={(e) => setLink(e.target.value)} value={link} type='text' className='bg-gray-50 text-black w-full rounded-lg text-base p-2 border border-gray-300 transition ease-in focus:outline-none' placeholder='URL' />
+              </div>
+              <button onClick={() => addLink()} type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-1.5 mr-2 mb-2">Add Link</button>
+              <hr className='my-1'></hr>
+            </div>
 
-          {renderLinks()}
+            <div className={`absolute ${add ? "animate__animated animate__slideInDown" : "top-0 animate__animated animate__slideInUp"}`}>
+              <div className='flex'>
+                {renderLinks()}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className='w-screen px-4 h-12 absolute bottom-4 left-0 z-30 animate__animated animate__fadeInUp'>
-        <div className='flex h-full rounded-lg' style={{backgroundColor: bgColorO}}>
+      <div className='w-screen md:px-4 h-12 absolute md:bottom-4 bottom-0 left-0 z-30 animate__animated animate__fadeInUp'>
+        <div className='flex h-full md:rounded-lg rounded-t-lg' style={{backgroundColor: bgColorO}}>
           <div className='flex items-center w-full justify-between px-4'>
             <div className='flex gap-3 md:w-64' style={{color: textColor}}>
               <p>{time}</p>
@@ -237,22 +267,29 @@ export default function Home() {
         </div>
       </div>
 
-      <div className={`max-w-sm h-96 w-full z-50 absolute bottom-20 right-4 rounded-lg p-4 bg-[#101010] text-[#f4f4f4] ${settings ? "block animate__animated animate__fadeInRight" : "animate__animated animate__fadeOutRight"}`}>
-        <div className='flex justify-between'>
+      <div className={`md:max-w-[350px] h-2/3 w-full z-50 absolute bottom-20 md:right-4 rounded-lg p-4 bg-white text-black ${settings ? "block animate__animated animate__fadeInRight" : "animate__animated animate__fadeOutRight"}`}>
+        <div className='flex justify-between h-10 bg-white z-30 sticky border-b-2 border-neutral-800'>
           <h1 className='text-2xl font-bold'>Settings</h1>
           <div className='flex gap-3'>
             <p className='cursor-pointer' onClick={() => saveState()}>Save</p>
             <p className='cursor-pointer' onClick={() => setDefault()}>Set default</p>
           </div>
         </div>
-        <hr className='my-1'></hr> 
-        <div className='space-y-4 overflow-y-scroll h-[300px] pr-2 py-4'>
+        <div className='space-y-4 overflow-y-scroll h-full pt-12 -mt-10 pr-2 py-4'>
           <div className='w-full pb-2'>
-            <p className='mb-2'>Background Image URL</p>
-            <input onChange={(e) => setURL(e.target.value)} value={url} type='text' className='bg-gray-50 w-full text-black rounded-lg text-base p-1 border border-gray-300 transition ease-in' placeholder='Hex Code' />
+            <p className='mb-2 font-medium'>Background Image</p>
+          
+            <div className='grid grid-cols-3 gap-1'>
+              <div onClick={() => setURL('https://images.unsplash.com/photo-1572816703439-d8b34c4dc93f')} className='h-16 bg-cover rounded-lg' style={{backgroundImage: "url('https://images.unsplash.com/photo-1572816703439-d8b34c4dc93f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80')"}}></div>
+              <div onClick={() => setURL('https://images.unsplash.com/photo-1477346611705-65d1883cee1e')} className='h-16 bg-cover rounded-lg' style={{backgroundImage: "url('https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80')"}}></div>
+              <div onClick={() => setURL('https://images.unsplash.com/photo-1509023464722-18d996393ca8')} className='h-16 bg-cover rounded-lg' style={{backgroundImage: "url('https://images.unsplash.com/photo-1509023464722-18d996393ca8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80')"}}></div>
+              <div onClick={() => setURL('https://images.unsplash.com/photo-1485470733090-0aae1788d5af')} className='h-16 bg-cover rounded-lg' style={{backgroundImage: "url('https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80')"}}></div>
+              <div onClick={() => setURL('https://images.unsplash.com/photo-1590091194890-7aa6462b82dd')} className='h-16 bg-cover rounded-lg' style={{backgroundImage: "url('https://images.unsplash.com/photo-1590091194890-7aa6462b82dd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80')"}}></div>
+              <div onClick={() => setURL('https://images.unsplash.com/photo-1643330683233-ff2ac89b002c')} className='h-16 bg-cover rounded-lg' style={{backgroundImage: "url('https://images.unsplash.com/photo-1643330683233-ff2ac89b002c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80')"}}></div>
+            </div>
           </div>
           <div className='w-full pb-4'>
-            <p className='mb-2'>Search Engine</p>
+            <p className='mb-2 font-medium'>Search Engine</p>
             <div className='md:grid md:grid-cols-3 text-sm gap-1'>
               <div className='p-2 hover:-translate-y-1 border cursor-pointer transition ease-in rounded-lg' onClick={() => setEngine('google')}>
                 <p className={`${engine == 'google' ? "font-bold" : ""}`}>Google</p>
